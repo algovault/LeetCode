41326048f23298c4d582dc61acea260c4e1822bb
Robert Smith <106425469+algovault@users.noreply.github.com>
2023-07-20 22:23:54 -0400

    Update leetcode1108.py

--- leetcode1108.py
+++ leetcode1108.py
@@ -1,13 +1,3 @@
-# class Solution:
-#     @classmethod
-#     def defangIPaddr(self, address) -> str:
-#         self.address = address
-#         y = "."
-#         if (address.find(".")>=1):
-#          print("test1")
-#          address = address.replace(".", "[.]")
-#          print(address)
-
 class Solution:
     @classmethod
     def defangIPaddr(self, address: str) -> str:
